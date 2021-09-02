b7bd22e6ba31ac72cee5d5d155b8eb1371a2a1f8
Ben Ramsey <ben@benramsey.com>
2021-09-02 09:59:46 -0500

    Also fetch RFCs from the wiki index

--- src/Wiki/Index.php
+++ src/Wiki/Index.php
@@ -12,6 +12,7 @@ use DOMXPath;
 use PhpRfcs\Wiki;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
 use RuntimeException;
 use Tidy;
 
@@ -29,44 +30,8 @@ class Index
      */
     public function getIndex(): array
     {
-        $request = $this->requestFactory
-            ->createRequest('GET', Wiki::RFC_BASE_URL)
-            ->withHeader('X-DokuWiki-Do', 'export_xhtmlbody');
-
-        $rfcPage = $this->httpClient->sendRequest($request);
-        $contents = $this->tidy->repairString($rfcPage->getBody()->getContents());
-
-        $dom = new DOMDocument();
-        @$dom->loadHTML('<?xml encoding="utf-8">' . $contents);
-
-        $xpath = new DOMXPath($dom);
-
-        // Find all <a> tags having a data-wiki-id attribute.
-        $links = $xpath->query('//a[@data-wiki-id]');
-
-        if (!$links instanceof DOMNodeList) {
-            throw new RuntimeException('Could not find index');
-        }
-
-        /** @var string[] $rfcs */
-        $rfcs = [];
-
-        foreach ($links as $link) {
-            $dataWikiId = $link->getAttribute('data-wiki-id');
-            if (str_starts_with($dataWikiId, 'rfc:')) {
-                $slug = substr($dataWikiId, strlen('rfc:'));
-
-                if (in_array($slug, array_column($rfcs, 'slug'))) {
-                    continue;
-                }
-
-                $rfcs[$slug] = [
-                    'slug' => $slug,
-                    'section' => $this->getSectionHeading($link),
-                    'url' => Wiki::RFC_BASE_URL . '/' . $slug,
-                ];
-            }
-        }
+        $rfcs = $this->addFromRfcsPage([]);
+        $rfcs = $this->addOrphanedRfcPages($rfcs);
 
         array_multisort(array_column($rfcs, 'slug'), SORT_ASC, $rfcs);
 
@@ -111,5 +76,59 @@ class Index
         }
 
         return trim(str_replace("\n", ' ', $element->nodeValue));
+    }
+
+    private function addFromRfcsPage(array $rfcs): array
+    {
+        $request = $this->requestFactory
+            ->createRequest('GET', Wiki::RFC_BASE_URL)
+            ->withHeader('X-DokuWiki-Do', 'export_xhtmlbody');
+
+        return $this->parseRfcs($request, $rfcs, true);
+    }
+
+    private function addOrphanedRfcPages(array $rfcs): array
+    {
+        $request = $this->requestFactory
+            ->createRequest('GET', Wiki::RFC_BASE_URL . '?do=index&idx=rfc');
+
+        return $this->parseRfcs($request, $rfcs);
+    }
+
+    private function parseRfcs(RequestInterface $request, array $rfcs, bool $inspectSection = false): array
+    {
+        $response = $this->httpClient->sendRequest($request);
+        $contents = $this->tidy->repairString($response->getBody()->getContents());
+
+        $dom = new DOMDocument();
+        @$dom->loadHTML('<?xml encoding="utf-8">' . $contents);
+
+        $xpath = new DOMXPath($dom);
+
+        // Find all <a> tags having a data-wiki-id attribute.
+        $links = $xpath->query('//a[@data-wiki-id]');
+
+        if (!$links instanceof DOMNodeList) {
+            throw new RuntimeException('Could not find data-wiki-id links on page');
+        }
+
+        foreach ($links as $link) {
+            $dataWikiId = $link->getAttribute('data-wiki-id');
+            if (str_starts_with($dataWikiId, 'rfc:')) {
+                $slug = substr($dataWikiId, strlen('rfc:'));
+
+                if (in_array($slug, array_column($rfcs, 'slug'))) {
+                    continue;
+                }
+
+                $rfcs[$slug] = [
+                    'slug' => $slug,
+                    'section' => $inspectSection ? $this->getSectionHeading($link) : 'Unknown',
+                    'url' => Wiki::RFC_BASE_URL . '/' . $slug,
+                ];
+            }
+        }
+
+        return $rfcs;
     }
 }
